0e44f34ea3d0a652f80ee9da7e88c74525d64366
Erion Berdyna <erion.berdyna@hotmail.com>
2023-03-08 19:51:09 +0100

    implement about us UI and CRUD reseravtion

---
+++ aboutus.php
@@ -0,0 +1,177 @@
+<?php
+        require_once 'includes/header.php';
+        require_once 'CRUD/Menu-controller.php';
+        ?>
+
+
+<?php
+    
+    session_start();
+    if(isset($_SESSION["user"])){
+      echo  '<li ><a class="nav-link" href="logout.php">LOG OUT</a></li>';
+    } else { echo ' <li ><a class="nav-link" href="login.php">LOG IN</a></li>';}
+       
+        if(isset($_SESSION["role"])){
+         echo '<li><a href="CRUD/display.php">DASHBOARD</a></li>';
+    }
+    ?>
+</ul>
+    </nav>
+    </div>
+        </header>
+        <br><br><br><br>
+        <section id="services">
+        <div class="container">
+            <h1>What Services we offer to our clients</h1>
+            <p class="random-txt">Who are in extremely love with eco friendly system.</p>
+            <div class="cards">
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-regular fa-user"></i>
+                    </span>
+                    <h5>Expert Technicians</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>                
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-solid fa-id-card"></i>
+                    </span>
+                    <h5>Professional Service</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-solid fa-phone"></i>
+                    </span>
+                    <h5>Great Support</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-solid fa-rocket"></i>
+                    </span>
+                    <h5>Technical Skills</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-regular fa-gem"></i>
+                    </span>
+                    <h5>Highly Recomended</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>
+                <div class="card">
+                    <span class="card-logo">
+                        <i class="fa-regular fa-comment"></i>
+                    </span>
+                    <h5>Positive Reviews</h5>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br> <br> <br> <br>
+            
+         <div class="content">           
+             <div class="text-content">
+     <?php
+        $cars = new Car;
+        $all = $cars->lexoDhenat();
+        for($i=0; $i<count($all) ;$i++){
+           $img1= $all[$i]['image'];
+           $img2= $all[++$i]['image'];
+           $img3= $all[++$i]['image'];
+           $img4= $all[++$i]['image'];
+          
+          
+           echo '<div class="slider">
+        
+            <div class="slides">
+                <div class="slide active">
+                    
+                    <img src="' . $img1.'" alt="image-1">
+                </div> 
+                
+                <div class="slide">  
+                    <img src="' . $img2.'" alt="image-2"> 
+                </div>
+                <div class="slide">
+                    <img src="' . $img3.'" alt="image-3">
+                </div>
+                <div class="slide">
+                    <img src="' . $img4.'" alt="image-4">
+                </div>
+            </div>
+
+
+            <!-- Controls -->
+            <div class="controls">
+                <button id="button-1" class="button">Image 1</button>
+                <button id="button-2" class="button">Image 2</button>
+                <button id="button-3" class="button">Image 3</button>
+                <button id="button-4" class="button">Image 4</button>
+            </div>
+      </div>';
+        break;
+
+
+        }
+
+        ?>
+        <br> <br> <br> <br>
+         
+            
+        <?php
+        require_once 'includes/footer.php';
+        ?>
+
+        
+<script>
+        let currentActive = 1;
+
+        const incrementActive = () => {
+            if (currentActive < 4) {
+                currentActive++;
+            } else {
+                currentActive = 1;
+            }
+        }
+
+        const showActiveSlide = () => {
+            const slides = document.querySelectorAll('.slide');
+
+            slides.forEach((slide, idx) => {
+                if (idx + 1 === currentActive) {
+                    slide.classList.add('active');
+                } else {
+                    slide.classList.remove('active');
+                }
+            });
+        }
+
+        const setAutoSlide = () => {
+            return setInterval(() => {
+                incrementActive();
+                showActiveSlide();
+            },4000)
+        }
+
+        let interval = setAutoSlide();
+         const buttons = document.querySelectorAll('.button');
+
+        buttons.forEach((button, idx) => {
+            button.addEventListener('click', () => {
+                clearInterval(interval);
+                // clearInterval
+
+                currentActive = idx + 1 ;
+                showActiveSlide();
+               // startInterval
+                interval = setAutoSlide()
+            });
+        });
+        
+    
+    </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"> 
+    <link rel="stylesheet" type="text/css" href="./assets/css/index.css" >
+    <link rel="stylesheet" href="./assets/css/header.css" >
+    <link rel="stylesheet" href="./assets/css/slider.css" >
+    <link rel= "stylesheet" href="../CRUD/displayStyle.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.jpg"> 
+    <title>Car Rental</title>
+</head>
+<body>
+<header>
+<nav class="nav">
+
+    <img src="assets/images/logo.jpg" alt="logo">
+       
+    
+    <ul class="nav-links">
+        
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="aboutus.php">ABOUT</a></li>
+        <li><a href="cars.php">CARS</a></li>
+        <li><a href="#services">SERVICE</a></li>
+        <!-- <li><a href="#">TEAM</a></li>
+        <li><a href="#blog">BLOG</a></li>
+        <li class="pages"><a href="#" >PAGES</a></li> -->
+        <li><a href="contact.php">CONTACT</a></li>
+       
+        
+   
+<!-- </nav>
+ <div class="content">           
+    <div class="text-content">
+        
+        <p>THE ROYAL ESSENCE OF JOURNEY</p>
+        <h1>RELAXED JOURNEY EVER</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="cars.php" ><button class="yellow first-button button">RENT A CAR NOW</button></a>
+    </div> -->
+    <!-- <div class="form-content">
+        <form method="GET" action="receivedOrder.html">
+            <h4>Book Your Car Today!</h4>
+            <select name="cars" placeholder="Select Your Car" required>
+                <option value="select">Select Your Car</option>
+                <option value="Mercedes">Mercedes</option>
+                <option value="Bmw">BMW</option>
+                <option value="Ferrari">Ferrari</option>
+                <option value="Toyota">Toyota</option>
+            </select>
+            <label for="pickup">Pick Up Date</label>
+            <input type="date" name="pickup" required>
+            <label for="drop">Drop Off Date</label>
+            <input type="date" name="drop" required>
+            <input type="text" placeholder="Your Name" required>
+            <input type="email" placeholder="Email address" required>
+            <input type="tel" placeholder="Phone number" maxlength="12" required>
+            <br>
+            <button class="yellow button">CONFIRM CAR BOOKING</button>
+        </form>
+    </div>
+</div>
+</header>  -->
