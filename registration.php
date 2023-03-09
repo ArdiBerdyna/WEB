a730cc0dfcdaa6c9c515bf4c7faf180029659421
ArdiBerdyna <Ardi_881@hotmail.com>
2023-03-09 04:23:51 +0100

    some changes

--- registration.php
+++ registration.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/Style_Form.css" >
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.jpg"> 
 
     <title>Registration</title>
 </head>
